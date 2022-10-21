4e6e5c63db615fc7b41665e272843008feb2b39f
Guilherme Lisboa <guibrbr161@gmail.com>
2022-10-21 15:45:15 -0300

    Funcionalidade nova

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from tasks.views import helloworld, taskList, yourName
+from tasks.views import helloworld, taskList, yourName, taskView
 
 
 
 urlpatterns = [
     path("helloworld/", helloworld),
     path('', taskList, name='task-list'),
+    path('task/<int:id>', taskView, name="task-view"),
     path('yourname/<str:name>', yourName, name='your-name')
 ]
