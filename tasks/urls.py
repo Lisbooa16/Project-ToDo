7d6aaf1491795bf6e81a35ebc0f1bfbec42cd3ec
Guilherme Lisboa <guibrbr161@gmail.com>
2022-10-21 16:04:44 -0300

    forms

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from tasks.views import helloworld, taskList, yourName, taskView
+from tasks.views import helloworld, taskList, yourName, taskView, newTask
 
 
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path("helloworld/", helloworld),
     path('', taskList, name='task-list'),
     path('task/<int:id>', taskView, name="task-view"),
+    path('newtask/', newTask, name="new-Task"),
     path('yourname/<str:name>', yourName, name='your-name')
 ]
